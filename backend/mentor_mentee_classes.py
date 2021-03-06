b473ef6fa0fab042f0203ee0f9c3e9d5024fe722
zareen <kabirz@mcmaster.ca>
2021-03-06 18:02:14 -0500

    Update mentor_mentee_classes.py

--- backend/mentor_mentee_classes.py
+++ backend/mentor_mentee_classes.py
@@ -5,52 +5,58 @@ class MatchingManager:
     mentees: list[Mentee]
 
     def __init__(self, mentors, mentees):
+        #list of mentors and mentees
         self.mentors = mentors
         self.mentees = mentees 
 
+        #empty lists that will hold matched and unmatched mentors and mentees
         self.matchedMentors = set()
         self.matchedMentees = set()
         self.unmatchedMentors = set()
         self.unmatchedMentees = set()
-        #self,Id,firstName,lastName,email, year,field_of_study,experiences, skills,  
+        self.unavailableMentors = set()
 
         #list of matched mentors and mentees and unmatched mentors
         for mentor in mentors:
+            #checks to see if mentor still has space
             if 0 <= len(mentor.mentees) < 2 :
-                    if matchedPair(mentor, mentee) == True
-                        self.matchedMentors.add(mentor)
-                        for mentee in mentor.mentees:
-                            self.matchedMentees.add(mentee)
-                else
-                    self.unmatchedMentors.add(mentor)
-            elif  len(mentor.mentees) = 2:
-                
-        # the list of unmatched mentees
+                for mentee in mentor.mentees:
+                    #case where both their fields and skills match
+                    if matchedPair(mentor, mentee) == 1:
+                        self.matchedMentors.append(mentor)
+                        self.matchedMentees.append(mentee)
+                    #case where only their fields match
+                    elif matchedPair(mentor,mentee) == 0.75:
+                        self.matchedMentors.append(mentor)
+                        self.matchedMentees.append(mentee)
+                    #case where neither match up
+                    else:
+                        self.unmatchedMentors.append(mentor)
+            #case where the mentor is at max capacity
+            elif  len(mentor.mentees) == 2:
+                self.unavailableMentors.append(mentor)                          
+        
+        #the list of unmatched mentees
         for mentee in self.mentees:
             if mentee not in self.matchedMentees:
                 self.unmatchedMentees.add(mentee)
-    
+    #people in unmatched categories will go into a waiting list
+
 
     def matchedPairs(mentor, mentee):
         #the experices of mentor match mentees
         #will be exchanged with weights later on
-
-        for interest in mentee.field_of_interests
-            if mentor.field_of_study == interests:
-                return True
-            for skills in mentee.skills_of_interest:
-
-        '''
-        if mentor previous_experiences match with mentee interest
-            then match them
-            return True
-        elif mentor field_of_study match with mentee interests
-            match them
-            return True
+        
+        if mentor.field_of_study == mentee.field_of_interest:
+            for mentor_skills in mentor.skills:
+                if mentor_skills == mentee.skills_of_interest: 
+                    return 1
+                else:
+                    pass
+            return 0.75
         else:
-            don't match them
-            return False
-        '''
+            return 0     
+
 class Mentor:
     """
     Precondition: 
@@ -78,14 +84,14 @@ class Mentor:
         # mentee: list[Mentee]
 
 class Mentee:
-    def __init__(self, netId, firstName,lastName,email, year,field_of_interests, skills_of_interest, mentor):
+    def __init__(self, netId, firstName,lastName,email, year,field_of_interest, skills_of_interest, mentor):
         self.Id = netId
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
         self.year = year
-        self.field_of_interests = list[field_of_interests]
-        self.skills_of_interest = list[skills_of_interest]
+        self.field_of_interest = field_of_interest
+        self.skills_of_interest = skills_of_interest
         self.mentor = Optional[Mentor]
 
         # mentee_name: str
@@ -98,14 +104,28 @@ class Mentee:
         # mentor = Optional[Mentor]
 
 
-
-
+        '''
+        if mentor previous_experiences match with mentee interest
+            then match them
+            return True
+        elif mentor field_of_study match with mentee interests
+            match them
+            return True
+        else:
+            don't match them
+            return False
+        '''
 
 
         '''
         weights:
-         if more than 2 skills and fields match: 100%
+         if more than 3 skills and fields match: 100%
             match that mentor and mentee together
-        elif (only skills match) or (only fields match): 50%
+        elif mote than 2 skills and fields match: 75%
+            they are a good match unless there are better combinations
+        elif (only  1 skill match) or (only fields match): 50%
             match them together if there arent any other better combinations
-        else neither match then try to find a better mentor 
+        else no match: 0%
+            neither match then try to find a better mentor
+            unless all are matches are the same
+        '''
