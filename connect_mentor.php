bf7b57b75ead2158003074d41879b22563ff0c19
faizahsayyid <faizah.sayyid@gmail.com>
2021-03-06 18:11:46 -0500

    started connecting forms to sql database

--- connect_mentor.php
+++ connect_mentor.php
@@ -10,4 +10,18 @@ if ($conn->connect_error){
 }
 echo "Connected succesfully";
 
+$first_name = $_POST{'fname'};
+$last_name = $_POST{'lname'};
+$email = $_POST{'email'};
+$experience_level = 'something';
+$skills = 'something';
+$field = 'something';
+$mentor = TRUE;
+
+
+$sql = "INSERT INTO application (first_name, last_name, email, experience_level, 
+                                skills, field, mentor)
+                VALUES ($first_name, $last_name, $email, $experience_level, 
+                        $skills, $mentor)"
+
 ?>
