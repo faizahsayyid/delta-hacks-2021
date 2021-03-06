9deb870e5d077ac97a2c154cd5f8c578eadcf551
zareen <kabirz@mcmaster.ca>
2021-03-06 11:31:26 -0500

    Update mentor_mentee_classes.py

--- backend/mentor_mentee_classes.py
+++ backend/mentor_mentee_classes.py
@@ -1,25 +1,38 @@
+from typing import Optional
+
 class MatchingManager:
     mentors: list[Mentor]
     mentees: list[Mentee]
 
-    def __init__(self):
-        mentors = []
-        mentees = []
+    def __init__(self, mentors, mentees):
+        self.mentors = []
+        self.mentees = []
         
 
 
 class Mentor:
     """
-
     Precondition: 
         - 1 <= len(mentee) <= 2
     """
-    schooling: str
-    field_of_stufy: str
-    skills: list[str]
-    mentee: list[Mentee]
+    def __init__(self, name, year,experiences,field_of_study, skills,  ):
+        
+        mentor_name: str
+        mentor_grade: str
+        mentor_email: str
+        schooling: str 
+        field_of_study: str
+        skills: list[str]
+        experiences : list[str]
+        mentee: list[Mentee]
 
 class Mentee:
-    schooling: str
-    field_of_stufy: str
-    skills: list[str]
+    def primary_mentee_info():
+        mentee_name: str
+        mentee_grade: str
+        mentee_email: str
+        schooling: str
+        field_of_study: str
+        skills: list[str]
+        experiences: list[str]
+        mentor = Optional[Mentor]
