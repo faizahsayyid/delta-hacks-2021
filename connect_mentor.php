033298e40feda1882e2e080daab6ef9c9c64c983
faizahsayyid <faizah.sayyid@gmail.com>
2021-03-06 20:56:52 -0500

    Application submission works!

--- connect_mentor.php
+++ connect_mentor.php
@@ -2,34 +2,35 @@
 $servername = 'localhost';
 $user = 'root';
 $pass = '';
+$dbname = 'deltahacks';
 
-$conn = new mysqli($servername, $user, $pass);
+$conn = new mysqli($servername, $user, $pass, $dbname);
 
 if ($conn->connect_error){
     die("Connection Failed: " . $conn->connect_error);
 }
 echo "Connected succesfully";
 
-$first_name = $_POST{"fname"};
-$last_name = $_POST{"lname"};
-$email = $_POST{"email"};
-$experience_level = 'something';
-$skills = 'something';
-$field = 'something';
+$first_name = $_REQUEST["fname"];
+$last_name = $_REQUEST["lname"];
+$email = $_REQUEST["email"];
+$experience_level = $_REQUEST["grade"];
+$skills = $_REQUEST["skills"];
+$field = $_REQUEST["field"];
 $mentor = TRUE;
 
 
-// $sql = "INSERT INTO application (first_name, last_name, email, experience_level, skills, field, mentor) 
-// VALUES ($first_name, $last_name, $email, $experience_level, $skills, $field, $mentor)";
+$sql = "INSERT INTO application (first_name, last_name, email, experience_level, skills, field, mentor) 
+VALUES ('$first_name', '$last_name', '$email', '$experience_level', '$skills', '$field', '$mentor')";
 
-// if ($conn->query($sql) === TRUE) {
-//     echo "Application Submitted";
-// } else {
-//     echo "Error: " . $sql . "<br>" . $conn->error;
-// }
+if ($conn->query($sql) === TRUE) {
+    echo "<br> Application Submitted";
+} else {
+    echo "<br> Error: " . $sql . "<br>" . $conn->error;
+}
 
+header("refresh:2; url=mentor_application.html");
 
-  
-// $conn->close();
+$conn->close();
 
 ?>
